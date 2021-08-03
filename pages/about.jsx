b821e4cf0589f7f82a387e3964185094ee372254
Marco Antonio González <mgonzalezdiez@gmail.com>
2021-08-02 21:10:33 -0400

    Primer commit de NextJs-Blog

---
+++ pages/about.jsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout"
+
+export default function about() {
+  return (
+
+    <Layout 
+      title="About | next.js"
+      description="Agregue una descripción"
+    >
+      <h1>About</h1>
+    </Layout>
+  )
+}
---
+++ styles/_app.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Layout.module.css';
+import Layout from "../components/Layout"
+import utilStyles from '../styles/utils.modules.css' 
+import '../style/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
